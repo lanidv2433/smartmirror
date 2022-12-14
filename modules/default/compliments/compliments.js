546ee96a5deb890f74aa5d21423bdad039bcf9b0
Lanni <63504496+lanidv2433@users.noreply.github.com>
2022-12-14 17:26:45 -0500

    Update compliments.js

--- modules/default/compliments/compliments.js
+++ modules/default/compliments/compliments.js
@@ -8,10 +8,10 @@ Module.register("compliments", {
 	// Module config defaults.
 	defaults: {
 		compliments: {
-			anytime: ["Hey there sexy!"],
-			morning: ["Good morning, handsome!", "Enjoy your day!", "How was your sleep?"],
-			afternoon: ["Hello, beauty!", "You look sexy!", "Looking good today!"],
-			evening: ["Wow, you look hot!", "You look nice!", "Hi, sexy!"],
+			anytime: ["Hey Lanni!"],
+			morning: ["Good morning, Lanni!", "Enjoy your day!", "Time to start the day!"],
+			afternoon: ["Hi!", "Looking good today!"],
+			evening: ["Good evening, Lanni!", "You look nice!"],
 			"....-01-01": ["Happy new year!"]
 		},
 		updateInterval: 30000,
